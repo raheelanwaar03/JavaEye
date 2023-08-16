2580df2f7aad31da05f6d5f13ffef0f2a8a2f247
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-08-16 18:08:11 +0500

    admin can see users

--- resources/views/admin/user/all.blade.php
+++ resources/views/admin/user/all.blade.php
@@ -17,9 +17,10 @@
                                             <th>Email</th>
                                             <th>Phone</th>
                                             <th>Referral</th>
+                                            <th>Amount</th>
                                             <th>Status</th>
                                             <th>Date</th>
-                                            <th>Amount</th>
+                                            <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -29,13 +30,17 @@
                                                 <td>{{ $user->email }}</td>
                                                 <td>{{ $user->phone }}</td>
                                                 <td>{{ $user->referral }}</td>
+                                                <td><strong>{{ $user->balance }}</strong></td>
                                                 @if ($user->status === 'pending')
                                                     <td><span class="badge light badge-primary">Pending</span></td>
                                                 @else
                                                     <td><span class="badge light badge-success">Approved</span></td>
                                                 @endif
                                                 <td>{{ $user->created_at }}</td>
-                                                <td><strong>{{ $user->balance }}</strong></td>
+                                                <td>
+                                                    <a href="#" class="btn btn-sm btn-success">Approved</a>
+                                                    <a href="#" class="btn btn-sm btn-danger">Rejected</a>
+                                                </td>
                                             </tr>
                                         @empty
                                             <h3>No User Registered yet!</h3>
