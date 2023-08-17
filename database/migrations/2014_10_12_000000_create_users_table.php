e915cdadd75cc13df11ff9c451b5cabdf47691b9
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-08-18 04:19:05 +0500

    af

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('role')->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('status');
+            $table->string('status')->default('pending');
             $table->rememberToken();
             $table->timestamps();
         });
