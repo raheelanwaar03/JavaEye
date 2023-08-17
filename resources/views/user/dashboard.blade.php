d8e6d21e3a47573a0dd56d382b6435224dc4c260
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-08-17 17:11:37 +0500

    new

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -25,20 +25,6 @@
                         </div>
                         <p class="small mt-2">Team</p>
                     </a>
-                    <a href="#" class="swiper-slide text-center">
-                        <div class="avatar avatar-60 no-shadow border-0">
-                            <div class="overlay bg-template"></div>
-                            <i class="material-icons text-template">assignment</i>
-                        </div>
-                        <p class="small mt-2">Bills</p>
-                    </a>
-                    <a href="#" class="swiper-slide text-center">
-                        <div class="avatar avatar-60 no-shadow border-0">
-                            <div class="overlay bg-template"></div>
-                            <i class="material-icons text-template">camera</i>
-                        </div>
-                        <p class="small mt-2">Scan</p>
-                    </a>
                 </div>
                 <div class="swiper-pagination"></div>
             </div>
@@ -65,7 +51,6 @@
                                 </div>
                             </div>
                         </div>
-
                         <div class="swiper-slide">
                             <div class="card shadow border-0 bg-template">
                                 <div class="card-body">
@@ -82,28 +67,44 @@
                                 </div>
                             </div>
                         </div>
-
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <div class="card shadow border-0 bg-template">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-auto pr-0">
+                                                <img src="{{ asset('asset/img/graphics-carousel-scheme1.png') }}"
+                                                    alt="" class="mw-100">
+                                            </div>
+                                            <div class="col align-self-center">
+                                                <h5 class="mb-2 font-weight-normal">Gold loan scheme</h5>
+                                                <p class="text-mute">Get all money at market rate of gold</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col text-center">
-                <h5 class="subtitle mb-1">Movie Tickets</h5>
-                <p class="text-secondary">Buy ticktes and earn daily profit</p>
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    <h5 class="subtitle mb-1">Movie Tickets</h5>
+                    <p class="text-secondary">Buy ticktes and earn daily profit</p>
+                </div>
             </div>
-        </div>
-        <div class="row text-center mt-4">
-            @forelse ($tickets as $ticket)
-            @csrf
-            <div class="col-6 col-md-3">
+            <div class="row text-center mt-4">
+                @forelse ($tickets as $ticket)
+                    @csrf
+                    <div class="col-6 col-md-3">
                         <div class="card shadow border-0 mb-3">
                             <div class="card-body">
                                 <div class="avatar avatar-60 no-shadow border-0">
                                     {{-- <div></div> --}}
-                                    <img src="{{ asset('images/'.$ticket->image) }}" alt="image" class="img-responsive" >
+                                    <img src="{{ asset('images/' . $ticket->image) }}" alt="image" class="img-responsive">
                                 </div>
                                 <h3 class="mt-3 mb-0 font-weight-normal">{{ $ticket->title }}</h3>
                                 <p class="text-secondary text-mute small">{{ $ticket->description }}</p>
@@ -113,48 +114,48 @@
                             </div>
                         </div>
                     </div>
-            @empty
-                <h3>No Ticket Available</h3>
-            @endforelse
-            {{ $tickets->withQueryString()->links('pagination::bootstrap-5') }}
+                @empty
+                    <h3>No Ticket Available</h3>
+                @endforelse
+                {{ $tickets->withQueryString()->links('pagination::bootstrap-5') }}
+            </div>
         </div>
-    </div>
 
-    <!-- footer-->
-    <div class="footer">
-        <div class="no-gutters">
-            <div class="col-auto mx-auto">
-                <div class="row no-gutters justify-content-center">
-                    <div class="col-auto">
-                        <a href="index.html" class="btn btn-link-default active">
-                            <i class="material-icons">home</i>
-                        </a>
-                    </div>
-                    <div class="col-auto">
-                        <a href="statistics.html" class="btn btn-link-default">
-                            <i class="material-icons">insert_chart_outline</i>
-                        </a>
-                    </div>
-                    <div class="col-auto">
-                        <a href="wallet.html" class="btn btn-link-default">
-                            <i class="material-icons">account_balance_wallet</i>
-                        </a>
-                    </div>
-                    <div class="col-auto">
-                        <a href="transactions.html" class="btn btn-link-default">
-                            <i class="material-icons">widgets</i>
-                        </a>
-                    </div>
-                    <div class="col-auto">
-                        <a href="profile.html" class="btn btn-link-default">
-                            <i class="material-icons">account_circle</i>
-                        </a>
+        <!-- footer-->
+        <div class="footer">
+            <div class="no-gutters">
+                <div class="col-auto mx-auto">
+                    <div class="row no-gutters justify-content-center">
+                        <div class="col-auto">
+                            <a href="index.html" class="btn btn-link-default active">
+                                <i class="material-icons">home</i>
+                            </a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="statistics.html" class="btn btn-link-default">
+                                <i class="material-icons">insert_chart_outline</i>
+                            </a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="wallet.html" class="btn btn-link-default">
+                                <i class="material-icons">account_balance_wallet</i>
+                            </a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="transactions.html" class="btn btn-link-default">
+                                <i class="material-icons">widgets</i>
+                            </a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="profile.html" class="btn btn-link-default">
+                                <i class="material-icons">account_circle</i>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- footer ends-->
+        <!-- footer ends-->
     </div>
 
     <!-- color chooser menu start -->
--- resources/views/user/team.blade.php
+++ resources/views/user/team.blade.php
@@ -8,22 +8,22 @@
             <div class="col-12 px-0">
                 <ul class="list-group list-group-flush border-top border-bottom">
                     @forelse ($users as $user)
-                    <li class="list-group-item">
-                        <div class="row align-items-center">
-                            <div class="col-auto pr-0">
-                                <div class="avatar avatar-50 no-shadow border-0">
-                                    <img src="{{ asset('asset/img/user3.png') }}" alt="avatar">
+                        <li class="list-group-item">
+                            <div class="row align-items-center">
+                                <div class="col-auto pr-0">
+                                    <div class="avatar avatar-50 no-shadow border-0">
+                                        <img src="{{ asset('asset/img/user3.png') }}" alt="avatar">
+                                    </div>
+                                </div>
+                                <div class="col align-self-center pr-0">
+                                    <h6 class="font-weight-normal mb-1">{{ $user->name }}</h6>
+                                    <p class="text-mute small text-secondary">{{ $user->created_at }}</p>
+                                </div>
+                                <div class="col-auto">
+                                    <h6 class="text-success">${{ $user->balance }}</h6>
                                 </div>
                             </div>
-                            <div class="col align-self-center pr-0">
-                                <h6 class="font-weight-normal mb-1">{{ $user->name }}</h6>
-                                <p class="text-mute small text-secondary">{{ $user->created_at }}</p>
-                            </div>
-                            <div class="col-auto">
-                                <h6 class="text-success">${{ $user->balance }}</h6>
-                            </div>
-                        </div>
-                    </li>
+                        </li>
                     @empty
                         <h3>You have not any team member</h3>
                     @endforelse
