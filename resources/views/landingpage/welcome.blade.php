0c36a607c5e1365a8b3f4e11e4c1b9706c54c6c1
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-08-23 03:35:43 +0500

    added

--- resources/views/landingpage/welcome.blade.php
+++ resources/views/landingpage/welcome.blade.php
@@ -275,7 +275,7 @@
             </div>
 
             <div class="col-4"
-                style="margin-left: 7px;width:100px;height: 100px;background-image: url('{{ asset('assets/img5.jpg') }}');border-radius: 10px !important;">
+                style="margin-left: 7px;width:100px;height: 100px;background-image: url('{{ asset('assets/img8.jpg') }}');border-radius: 10px !important;">
                 <a href="{{ route('User.Dashboard') }}">
                     <p class="card-text" style="font-size: 11px;margin-top: 70px;"><span
                             style="padding: 8px 10px;color: white;border-radius: 10px;background-color: blue;">0$</span><span
@@ -284,6 +284,17 @@
                 </a>
 
             </div>
+
+            <div class="col-4"
+            style="margin-left: 7px;width:100px;height: 100px;background-image: url('{{ asset('assets/img9.jpg') }}');border-radius: 10px !important;">
+            <a href="{{ route('User.Dashboard') }}">
+                <p class="card-text" style="font-size: 11px;margin-top: 70px;"><span
+                        style="padding: 8px 10px;color: white;border-radius: 10px;background-color: blue;">0$</span><span
+                        style="float: right;padding: 2px 10px;color: white;border-radius: 10px;background-color: red;">Buy</span>
+                </p>
+            </a>
+
+        </div>
 
         </div>
 
