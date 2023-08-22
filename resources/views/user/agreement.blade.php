ded775cc9c6ab15583ff37ff967770b5d591125f
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-08-23 04:03:53 +0500

    user agreement

--- resources/views/user/agreement.blade.php
+++ resources/views/user/agreement.blade.php
@@ -1,1 +1,99 @@
-User agreement
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Agreement - Online Investing</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f7f7f7;
+        }
+
+        header {
+            background-color: #007bff;
+            color: white;
+            text-align: center;
+            padding: 1rem;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 2rem;
+            background-color: white;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+            margin-top: 2rem;
+        }
+
+        h1 {
+            margin-top: 0;
+        }
+
+        p {
+            line-height: 1.6;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <h1>User Agreement</h1>
+        <p>Welcome to our Online Investing platform. Please read the following agreement carefully.</p>
+    </header>
+    <!-- ... Previous HTML code ... -->
+
+    <div class="container">
+        <h2>1. Introduction</h2>
+        <p>This User Agreement ("Agreement") is a legal contract between you and [Your Company Name], governing your use
+            of our online investing services.</p>
+
+        <h2>2. Acceptance of Terms</h2>
+        <p>By accessing or using our platform, you agree to be bound by the terms and conditions outlined in this
+            Agreement.</p>
+
+        <h2>3. Eligibility</h2>
+        <p>You must be at least 18 years old and have the legal capacity to enter into this Agreement. You also agree to
+            provide accurate and truthful information.</p>
+
+        <h2>4. Investment Risks</h2>
+        <p>Investing in financial markets carries inherent risks. You acknowledge that the value of investments can both
+            rise and fall, and you are solely responsible for your investment decisions.</p>
+
+        <h2>5. Account Security</h2>
+        <p>You are responsible for maintaining the security of your account credentials. Notify us immediately of any
+            unauthorized access or suspicious activities.</p>
+
+        <h2>6. Fees and Charges</h2>
+        <p>Our platform may charge fees for certain transactions and services. These fees will be clearly communicated
+            to you before you proceed.</p>
+
+        <h2>7. Termination</h2>
+        <p>We reserve the right to suspend or terminate your account if you violate the terms of this Agreement or
+            engage in fraudulent or harmful activities.</p>
+
+        <h2>8. Limitation of Liability</h2>
+        <p>We are not liable for any losses or damages arising from your use of our platform. You acknowledge that
+            investments carry risks and you assume all responsibility.</p>
+
+        <h2>9. Governing Law</h2>
+        <p>This Agreement is governed by and construed in accordance with the laws of [Your Jurisdiction]. Any disputes
+            will be subject to the exclusive jurisdiction of the courts in [Your Jurisdiction].</p>
+
+        <h2>10. Changes to the Agreement</h2>
+        <p>We reserve the right to modify this Agreement at any time. Any changes will be communicated to you through
+            the platform or by email.</p>
+
+        <h2>11. Contact Us</h2>
+        <p>If you have any questions about this Agreement, please contact us at [contact@yourcompany.com].</p>
+    </div>
+
+    <!-- ... Rest of the HTML code ... -->
+
+</body>
+
+</html>
