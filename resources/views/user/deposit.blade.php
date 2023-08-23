8448d9789bbfc2b4bed142e97182ee204cad04db
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-08-24 02:12:28 +0500

    new addition

--- resources/views/user/deposit.blade.php
+++ resources/views/user/deposit.blade.php
@@ -1,26 +1,63 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
 
-@section('content')
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="theme-color" content="#000000">
+    <title>{{ env('APP_NAME') }}</title>
+    <meta name="description" content="Work at home">
+    <meta name="keywords" content="Online working at home" />
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}" sizes="32x32">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/icon/192x192.png') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('font-awesome/css/font-awesome.min.css') }}">
+</head>
+
+<body>
+
+    <!-- App Header -->
+    <div class="appHeader text-light">
+        <div class="left">
+            <h4 style="color:white">Recharge</h4>
+        </div>
+        <div class="right">
+            <!-- <h4><a href="mine.html"><i class="fa fa-user-circle" aria-hidden="true" style="color: white;"></i></a></h4> -->
+        </div>
+
+    </div>
+    <!-- * App Header -->
+
+    <!-- App Capsule -->
     <div id="appCapsule">
 
         <center>
-            <div class="blog-card mb-3" style="border-radius: 10px !important;padding: 5px;width: 200px;height:70px;">
-                <p class="card-text" style="font-size: 11px;color: rgb(64, 64, 151);">{{ auth()->user()->balance }}</p>
-                <p class="card-text" style="margin-top: -20px; font-size: 11px; color: black !important;">Current balance</p>
+            <div style="padding: 10px;">
+                <div class="blog-card" style="border-radius: 10px !important;">
+                    <p class="card-text" style="font-size: 11px;color: rgb(64, 64, 151);">{{ auth()->user()->balance }}
+                    </p>
+                    <p class="card-text" style="margin-top: -20px; font-size: 11px; color: black !important;">Current
+                        balance</p>
+                </div>
             </div>
         </center>
+
         <div class="col-12">
             <div style="border: 1px solid black; border-radius: 5px !important;padding: 15px;">
                 <form action="{{ route('User.Store.Deposit.Money') }}" method="POST">
                     @csrf
                     <h3 class="text-center text-white">Deposit Money</h3>
-                    <p class="card-text" style="margin-top:-10px;"><span style="color: white;">Amount To Deposit:</span></p>
-                    <input placeholder="Enter Amount you want to deposit" type="number" name="amount"
+                    <p class="card-text" style="margin-top:-10px;"><span style="color: white;">Amount To Deposit:</span>
+                    </p>
+                    <input placeholder="Enter Amount" type="number" name="amount"
                         style="background: #040e2b;border:none;color:white;">
                     <hr>
 
                     <p class="card-text" style="margin-top:-10px;"><span style="color: white;">Account Title:</span></p>
-                    <input placeholder="Your Account Title" type="text" name="account_title"
+                    <input placeholder="Account Title" type="text" name="account_title"
                         style="background: #040e2b;border:none;color:white;">
                     <hr>
 
@@ -30,7 +67,8 @@
                     <hr>
 
                     <div class="d-flex justify-content-between align-items-center">
-                        <p class="card-text" style="margin-top:-10px;"><span style="color: white;">Our Trc20 Id:</span></p>
+                        <p class="card-text" style="margin-top:-10px;"><span style="color: white;">Our Trc20 Id:</span>
+                        </p>
                         <a class="btn btn-sm btn-danger ml-3 text-white" onclick="copy()">Click to Copy</a>
                     </div>
                     <input id="myInput" value="TYjaBbDLPakArN7pUAptMG1KdqAQGjUCQg" readonly
@@ -45,9 +83,20 @@
 
         <center>
             <div style="padding: 10px;">
-                <button type="submit" class="btn btn-danger">Submit</button>
+                <a class="btn btn-danger">Confirm Recharge</a>
             </div>
         </center>
         </form>
     </div>
-@endsection
+
+
+    <script src="{{ asset('assets/js/jquery-3.2.1.min.js') }}"></script>
+    <!-- Bootstrap -->
+    <script src="{{ asset('assets/js/lib/bootstrap.bundle.min.js') }}"></script>
+    <!-- Ionicons -->
+    <script type="module" src="{{ asset('assets/js/ionicons.js') }}"></script>
+    <!-- Splide -->
+    <script src="{{ asset('assets/js/plugins/splide/splide.min.js') }}"></script>
+</body>
+
+</html>
